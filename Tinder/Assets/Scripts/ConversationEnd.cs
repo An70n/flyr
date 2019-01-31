a7ba71bba931467f6277445ffcc13faeac9459ed
Romain Dannin <romain.dannin@gmail.com>
2019-01-31 10:32:08 +0100

    gestin fin conversations

---
+++ Tinder/Assets/Scripts/ConversationEnd.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConversationEnd : TextlineDialogueUI
+{
+    public override void Close()
+    {
+        OnRecordPersistentData();
+        base.Close();
+        Debug.Log("conversation end");
+    }
+}
