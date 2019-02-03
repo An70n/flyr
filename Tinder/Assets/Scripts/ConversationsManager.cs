2493d60bc70fa682bc7ed640029f7027c53b997c
Romain Dannin <romain.dannin@gmail.com>
2019-02-03 11:46:09 +0100

    nettoyage code 2

--- Tinder/Assets/Scripts/ConversationsManager.cs
+++ Tinder/Assets/Scripts/ConversationsManager.cs
@@ -1,18 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; 
 
 public class ConversationsManager : MonoBehaviour
 {
+
+    [SerializeField] private Color j_Color;
+    [SerializeField] private Color k_Color;
+    [SerializeField] private Color r_Color;
+
     [SerializeField] private GameObject appMenu;
 
     [SerializeField] private GameObject[] j_History;
     [SerializeField] private GameObject[] k_History;
     [SerializeField] private GameObject[] r_History;
 
+    [SerializeField] private Text[] j_History_lines;
+    [SerializeField] private Text[] k_History_lines;
+    [SerializeField] private Text[] r_History_lines;
+
     private bool jHistory_activated = false;
     private bool kHistory_activated = false;
     private bool rHistory_activated = false;
+
+    [SerializeField] private Text nPCmessageText;
+    [SerializeField] private Text headingText;
+
+
+
 
     public void OpenDialogueR()
     {
@@ -36,6 +52,14 @@ public class ConversationsManager : MonoBehaviour
             {foreach (GameObject rH in r_History)
                 {rH.SetActive(true);
                     rHistory_activated = true;}}
+        }
+
+        nPCmessageText.color = r_Color;
+        headingText.text = "R";
+
+        foreach (Text rH in r_History_lines)
+        {
+            rH.color = r_Color;
         }
     }
 
@@ -62,6 +86,14 @@ public class ConversationsManager : MonoBehaviour
                 {jH.SetActive(true);
                     jHistory_activated = true;}}
         }
+
+        nPCmessageText.color = j_Color;
+        headingText.text = "J";
+
+        foreach (Text jH in j_History_lines)
+        {
+            jH.color = j_Color;
+        }
     }
 
     public void OpenDialogueK()
@@ -86,6 +118,14 @@ public class ConversationsManager : MonoBehaviour
             {foreach (GameObject kH in k_History)
                 {kH.SetActive(true);
                     kHistory_activated = true;}}
+        }
+
+        nPCmessageText.color = k_Color;
+        headingText.text = "K";
+
+        foreach (Text kH in k_History_lines)
+        {
+            kH.color = k_Color;
         }
     }
 
