17ba5cc953d197c7549c3bac24d43f49b292a051
Romain Dannin <romain.dannin@gmail.com>
2019-02-04 14:19:34 +0100

    système couleurs

--- Tinder/Assets/Scripts/ConversationsManager.cs
+++ Tinder/Assets/Scripts/ConversationsManager.cs
@@ -20,28 +20,46 @@ public class ConversationsManager : MonoBehaviour
     [SerializeField] private Text[] k_History_lines;
     [SerializeField] private Text[] r_History_lines;
 
-    private bool jHistory_activated = false;
-    private bool kHistory_activated = false;
-    private bool rHistory_activated = false;
+    [SerializeField] private bool jHistory_activated = false;
+    [SerializeField] private bool kHistory_activated = false;
+    [SerializeField] private bool rHistory_activated = false;
 
-    [SerializeField] private Text nPCmessageText;
     [SerializeField] private Text headingText;
 
+    [SerializeField] private Transform player;
+    [SerializeField] private Transform J;
+    [SerializeField] private Transform K;
+    [SerializeField] private Transform R; 
 
-
+    //private Text nPCmessageText;
+    //private GameObject nPCMessage; 
 
     public void OpenDialogueR()
     {
+        //nPCmessageText.color = r_Color; 
+        //Debug.Log("text is now blue");
+        headingText.text = "R";
+
+        foreach (Text rH in r_History_lines)
+        {
+            rH.color = r_Color;
+        }
+
         if (PixelCrushers.DialogueSystem.DialogueManager.ConversationHasValidEntry("R"))
        
         {
-            PixelCrushers.DialogueSystem.DialogueManager.StartConversation("R");
+            PixelCrushers.DialogueSystem.DialogueManager.StartConversation("R", player, R);
             appMenu.SetActive(false);
 
             if (rHistory_activated == false)
             {foreach (GameObject rH in r_History)
                 {rH.SetActive(true);
                     rHistory_activated = true;}}
+
+            foreach (Text rH in r_History_lines)
+            {
+                rH.color = r_Color;
+            }
         }
         else if (!PixelCrushers.DialogueSystem.DialogueManager.ConversationHasValidEntry("R"))
         {
@@ -52,29 +70,40 @@ public class ConversationsManager : MonoBehaviour
             {foreach (GameObject rH in r_History)
                 {rH.SetActive(true);
                     rHistory_activated = true;}}
+
+            foreach (Text rH in r_History_lines)
+            {
+                rH.color = r_Color;
+            }
         }
 
-        nPCmessageText.color = r_Color;
-        headingText.text = "R";
-
-        foreach (Text rH in r_History_lines)
-        {
-            rH.color = r_Color;
-        }
+       
     }
 
     public void OpenDialogueJ()
     {
+        //nPCMessage = GameObject.Find("NPC Message Template");
+        //nPCMessage.GetComponentInChildren<Text>(true).color = j_Color; 
+        //Debug.Log("text is now red");
+        //nPCmessageText.color = j_Color;
+        headingText.text = "J";
+
+        foreach (Text jH in j_History_lines)
+        {
+            jH.color = j_Color;
+        }
+
         if (PixelCrushers.DialogueSystem.DialogueManager.ConversationHasValidEntry("J"))
 
         {
-            PixelCrushers.DialogueSystem.DialogueManager.StartConversation("J");
+            PixelCrushers.DialogueSystem.DialogueManager.StartConversation("J", player, J);
             appMenu.SetActive(false);
 
             if (jHistory_activated == false)
             {foreach (GameObject jH in j_History)
                 {jH.SetActive(true);
                     jHistory_activated = true;}}
+
         }
         else if (!PixelCrushers.DialogueSystem.DialogueManager.ConversationHasValidEntry("J"))
         {
@@ -85,31 +114,37 @@ public class ConversationsManager : MonoBehaviour
             {foreach (GameObject jH in j_History)
                 {jH.SetActive(true);
                     jHistory_activated = true;}}
-        }
 
-        nPCmessageText.color = j_Color;
-        headingText.text = "J";
-
-        foreach (Text jH in j_History_lines)
-        {
-            jH.color = j_Color;
         }
     }
 
     public void OpenDialogueK()
     {
+
+        //GameObject.Find("NPC Message Template").GetComponentInChildren<Text>(true).color = k_Color;
+        //Debug.Log("text is now green");
+        //GameObject.Find("Dialogue UI").transform.Find("NPC Message Template").GetComponent<Text>().color = k_Color; 
+        //nPCmessageText.color = k_Color; 
+        headingText.text = "K";
+
+        foreach (Text kH in k_History_lines)
+        {
+            kH.color = k_Color;
+        }
+
         if (PixelCrushers.DialogueSystem.DialogueManager.ConversationHasValidEntry("K"))
 
         {
-            PixelCrushers.DialogueSystem.DialogueManager.StartConversation("K");
+            PixelCrushers.DialogueSystem.DialogueManager.StartConversation("K", player, K);
             appMenu.SetActive(false);
 
             if (kHistory_activated == false)
             {foreach (GameObject kH in k_History)
                 {kH.SetActive(true);
                     kHistory_activated = true;}}
+
         }
-       else if (!PixelCrushers.DialogueSystem.DialogueManager.ConversationHasValidEntry("K"))
+        else if (!PixelCrushers.DialogueSystem.DialogueManager.ConversationHasValidEntry("K"))
         {
             PixelCrushers.DialogueSystem.DialogueManager.dialogueUI.Open();
             appMenu.SetActive(false);
@@ -118,14 +153,6 @@ public class ConversationsManager : MonoBehaviour
             {foreach (GameObject kH in k_History)
                 {kH.SetActive(true);
                     kHistory_activated = true;}}
-        }
-
-        nPCmessageText.color = k_Color;
-        headingText.text = "K";
-
-        foreach (Text kH in k_History_lines)
-        {
-            kH.color = k_Color;
         }
     }
 
@@ -168,4 +195,5 @@ public class ConversationsManager : MonoBehaviour
             {rH.SetActive(false);
                 rHistory_activated = false;}}
     }
+
 }
