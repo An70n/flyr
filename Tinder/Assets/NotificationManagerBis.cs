10f9fa540f39674229e32ea821868596865921ab
Romain Dannin <romain.dannin@gmail.com>
2019-02-08 18:18:11 +0100

    walkthrough

---
+++ Tinder/Assets/NotificationManagerBis.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class NotificationManagerBis : MonoBehaviour
+{
+    public Text nPC;
+    public Text message;
+
+    private void Update()
+    {
+        if (PixelCrushers.DialogueSystem.DialogueLua.GetVariable("ConversationsDone").asInt == 3)
+        {
+            nPC.text = "Mom";
+            message.text = "How are you?";
+            StartCoroutine(Notification());
+        }
+    }
+
+    private void PlayNotification()
+    {
+        this.gameObject.GetComponentInChildren<Animator>().SetTrigger("notifPlaying");
+
+    }
+
+    private IEnumerator Notification()
+    {
+        PlayNotification();
+        yield return new WaitForSeconds(1f);
+        this.enabled = false;
+    }
+}
