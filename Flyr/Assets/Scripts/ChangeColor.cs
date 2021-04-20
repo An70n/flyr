59ad621d5aa73b1fe210c4304ce195b9b6c82193
Romain Dannin <romain.dannin@gmail.com>
2021-04-20 10:44:16 +0200

    dialogue pc bubbles color fixed

--- Flyr/Assets/Scripts/ChangeColor.cs
+++ Flyr/Assets/Scripts/ChangeColor.cs
@@ -9,33 +9,12 @@ public class ChangeColor : MonoBehaviour
     public Color flyrColor;
     public Color messagesColor;
 
-    public GameObject appMenu;
-    public GameObject messagesMenu;
-
     public Highlight firstResponseHighlight, secondResponseHighlight, thirdResponseHighlight; 
 
     public Image pcMessageBgd;
     public Unity.VectorGraphics.SVGImage firstChoice, secondChoice, thirdChoice; 
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        /*if(appMenu.activeInHierarchy)
-        {
-            pcMessageBgd.color = flyrColor;
-        }else if(messagesMenu.activeInHierarchy)
-        {
-            pcMessageBgd.color = messagesColor;
-        }*/
-    }
-
-    public void ChangeToAppColor()
+    public void ChangeToFlyrColor()
     {
         pcMessageBgd.color = flyrColor;
         firstChoice.color = flyrColor;
