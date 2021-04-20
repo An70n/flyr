a855698157d4e858936917e83f006d4044a46471
Romain Dannin <romain.dannin@gmail.com>
2021-04-20 10:37:16 +0200

    notifications are now working

--- Flyr/Assets/Scripts/NotificationManager.cs
+++ Flyr/Assets/Scripts/NotificationManager.cs
@@ -6,7 +6,7 @@ public class NotificationManager : MonoBehaviour
 {
     private void Update()
     {
-        if(PixelCrushers.DialogueSystem.DialogueLua.GetVariable("rConversationStart").asBool == true)
+        if(PixelCrushers.DialogueSystem.DialogueLua.GetVariable("notificationAlertR").asBool == true)
         {
             string r_History = "3;3;46;3;47;3;3";
             PixelCrushers.DialogueSystem.DialogueLua.SetVariable("DialogueEntryRecords_R", r_History);
@@ -17,9 +17,10 @@ public class NotificationManager : MonoBehaviour
             GameObjectsList.gameObjectsList.r_preview.fontStyle = TMPro.FontStyles.Bold;
 
             PlayNotification();
+            PixelCrushers.DialogueSystem.DialogueLua.SetVariable("notificationAlertR", false); 
         }
 
-        if (PixelCrushers.DialogueSystem.DialogueLua.GetVariable("ConversationsDone").asInt == 3)
+        if (PixelCrushers.DialogueSystem.DialogueLua.GetVariable("notificationAlertMom").asInt == 3)
         {
             string mom_History = "7;4;19;4;20;4;21;4;22;4;23;4;24;4;1";
             PixelCrushers.DialogueSystem.DialogueLua.SetVariable("DialogueEntryRecords_Mom", mom_History);
@@ -30,6 +31,7 @@ public class NotificationManager : MonoBehaviour
             GameObjectsList.gameObjectsList.mom_preview.fontStyle = TMPro.FontStyles.Bold;
 
             PlayNotification();
+            PixelCrushers.DialogueSystem.DialogueLua.SetVariable("notificationAlertMom", 0);
         }
     }
 
